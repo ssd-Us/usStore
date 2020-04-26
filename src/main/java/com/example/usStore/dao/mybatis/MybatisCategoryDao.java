921c07a70bf5f7078c8d3d9149445d076f9760a7
o3x1998 <munjeong0719@gmail.com>
2020-04-26 20:49:55 +0900

    add:mybatis

---
+++ src/main/java/com/example/usStore/dao/mybatis/MybatisCategoryDao.java
@@ -0,0 +1,29 @@
+package com.example.usStore.dao.mybatis;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+import com.example.usStore.dao.CategoryDao;
+import com.example.usStore.dao.mybatis.mapper.CategoryMapper;
+import com.example.usStore.domain.Category;
+
+@Repository
+public class MybatisCategoryDao implements CategoryDao {
+	@Autowired
+	private CategoryMapper categoryMapper;
+	
+	public List<Category> getCategoryList() throws DataAccessException {
+		return categoryMapper.getCategoryList();
+	}
+
+	public Category getCategory(String categoryId) throws DataAccessException {
+		return categoryMapper.getCategory(categoryId);
+	}
+
+	@Override
+	public Category getCategory(int categoryId) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/main/java/com/example/usStore/dao/mybatis/MybatisOrderDao.java
@@ -0,0 +1,53 @@
+package com.example.usStore.dao.mybatis;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.usStore.dao.OrderDao;
+import com.example.usStore.dao.SequenceDao;
+import com.example.usStore.dao.mybatis.mapper.LineItemMapper;
+import com.example.usStore.dao.mybatis.mapper.OrderMapper;
+import com.example.usStore.domain.LineItem;
+import com.example.usStore.domain.Order;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MybatisOrderDao implements OrderDao {
+	
+	@Autowired
+	protected OrderMapper orderMapper;
+	@Autowired
+	protected LineItemMapper lineItemMapper;
+	@Autowired
+	private SequenceDao sequenceDao;
+
+	public List<Order> getOrdersByUsername(String username) 
+			throws DataAccessException {
+	    return orderMapper.getOrdersByUsername(username);
+	}
+	
+	@Transactional
+	public Order getOrder(int orderId) throws DataAccessException {
+		Order order = orderMapper.getOrder(orderId);
+		if (order != null) {
+			order.setLineItems(lineItemMapper.getLineItemsByOrderId(orderId));
+		}
+	    return order;
+	}
+	
+	@Transactional
+	public void insertOrder(Order order) throws DataAccessException {  
+    	order.setOrderId(sequenceDao.getNextId("ordernum"));
+    	orderMapper.insertOrder(order);
+    	orderMapper.insertOrderStatus(order);
+    	for (int i = 0; i < order.getLineItems().size(); i++) {
+    		LineItem lineItem = (LineItem) order.getLineItems().get(i);
+    		lineItem.setOrderId(order.getOrderId());
+    		lineItemMapper.insertLineItem(lineItem);
+    	}
+	}
+}
