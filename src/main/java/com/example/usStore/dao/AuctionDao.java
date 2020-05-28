7da81b0a894cdae6810cf6f7e3250ce6342218b2
o3x1998 <munjeong0719@gmail.com>
2020-05-28 20:23:08 +0900

    [fix] : AuctionDao.java 수정 전으로 다시 돌림.

--- src/main/java/com/example/usStore/dao/AuctionDao.java
+++ src/main/java/com/example/usStore/dao/AuctionDao.java
@@ -7,18 +7,13 @@ import org.springframework.dao.DataAccessException;
 import com.example.usStore.domain.Auction;
 
 public interface AuctionDao {
-	// selete Auction
 	List<Auction> getAuctionList() throws DataAccessException;
-
-	// insert Auction
+	
+	Auction getAuctionItem(int itemId) throws DataAccessException;
+	
 	public void insertAuction(Auction auction) throws DataAccessException;
-
-	// delete Auction
-	public void deleteAuction(int itemId) throws DataAccessException;
-
-	// update Auction
+	  
+	public void delteAuction(Auction auction) throws DataAccessException;
+	  
 	public void updateAuction(Auction auction) throws DataAccessException;
-
-	//아이템 아이디로 해당 경매 객체 반환
-	public Auction getAuctionById(int itemId) throws DataAccessException;
 }
