b7027b4407e2250de8c3c7d8e9d67dba8b8a8949
jingni <48404998+leejieuns2@users.noreply.github.com>
2020-06-09 02:48:11 +0900

    [fix] TagDao 수정

--- src/main/java/com/example/usStore/dao/mybatis/mapper/TagMapper.java
+++ src/main/java/com/example/usStore/dao/mybatis/mapper/TagMapper.java
@@ -9,13 +9,13 @@ import com.example.usStore.domain.Tag;
 public interface TagMapper {
 
 	//아이템 아이디로 태그 객체 찾기
-	List<Tag> getTagList();
+	List<Tag> getTagList ();
 
 	// 태그 이름으로 태그 객체 찾기(검색 기능에 이용?)
-	Tag getTagByTagId(int tagId);
+	List<Tag> getTagByTagId(int tagId);
 
 	// itemId로 태그 객체 찾기
-	Tag getTagByItemId(int itemId);	
+	List<Tag> getTagByItemId(int itemId);	
 
 	// tagName으로 태그 객체 찾기
 	Tag getTagByTagName(String tagName);	
