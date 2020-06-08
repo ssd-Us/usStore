a83ca07de81eca8fc30f1faad9023db3fda36a76
jingni <48404998+leejieuns2@users.noreply.github.com>
2020-06-09 01:50:16 +0900

    [fix] Mapper 구현중

--- src/main/java/com/example/usStore/dao/mybatis/mapper/AttendanceMapper.java
+++ src/main/java/com/example/usStore/dao/mybatis/mapper/AttendanceMapper.java
@@ -9,7 +9,7 @@ import com.example.usStore.domain.Attendance;
 public interface AttendanceMapper {
 
 	// 달별로 매개변수로 전달받은 사용자가 출석체크를 한 날짜들의 리스트를 받아오는 메소드
-	List<Attendance> getAttendanceList(String user_id);
+	List<Attendance> getAttendanceList(String userId);
 	
 	// 사용자가 출석체크를 하면 실행되는 메소드. DB에 출석체크 날짜와 사용자 id 입력
 	void insertAttendance(Attendance attendance);
