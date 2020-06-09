886870a2be44b97af751352f73dad7502f4bf274
angelatto <pudri109@gmail.com>
2020-06-09 22:16:57 +0900

    [fix] secondHand.jsp에서 게시물 목록 출력

--- src/main/java/com/example/usStore/controller/item/SecondHandFormController.java
+++ src/main/java/com/example/usStore/controller/item/SecondHandFormController.java
@@ -35,7 +35,7 @@ public class SecondHandFormController {
 		PagedListHolder<SecondHand> itemList = new PagedListHolder<SecondHand>(this.itemFacade.getSecondHandList());
 		itemList.setPageSize(4);
 		
-		model.put("secondHandList", itemList);
+		model.put("itemList", itemList);
 		return "Product/secondHand"; //뷰 네임은 그 목록 보여주는 페이지 
 	}
 	
