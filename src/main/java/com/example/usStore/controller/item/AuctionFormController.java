c491af6a7f5804e5615ad6f7f47052a93f8e524e
o3x1998 <munjeong0719@gmail.com>
2020-06-09 21:44:57 +0900

    [add] : AuctionFormController 에 필요한 메소드 틀만 생성

--- src/main/java/com/example/usStore/controller/item/AuctionFormController.java
+++ src/main/java/com/example/usStore/controller/item/AuctionFormController.java
@@ -3,6 +3,7 @@ package com.example.usStore.controller.item;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.example.usStore.domain.Auction;
 import com.example.usStore.domain.Tag;
 import com.example.usStore.service.facade.ItemFacade;
 
+@Controller
 public class AuctionFormController {
    private ItemFacade itemFacade;
    
@@ -21,7 +23,7 @@ public class AuctionFormController {
    }
    
    //경매 리스트
-   @RequestMapping("/shop/viewProduct.do") 
+   @RequestMapping("/shop/auction/listItem.do") 
    public String auctionList(@RequestParam("productId") int productId, ModelMap model) {
 //      List<Auction> auctionList = this.itemFacade.getAuctionList();
 //      
@@ -34,7 +36,7 @@ public class AuctionFormController {
    }
    
    //경매 상세 페이지
-   @RequestMapping("/shop/auctionView") 
+   @RequestMapping("/shop/auction/viewItem.do") 
    public String auctionView(@RequestParam("itemId") int itemId, ModelMap model) {
       Auction auction = this.itemFacade.getAuctionById(itemId);
       List<Tag> tag = this.itemFacade.getTagByItemId(itemId);
@@ -48,12 +50,28 @@ public class AuctionFormController {
       
       return "Product/viewAuction";
    }
-   
-   //경매 수정
-   //경매 삭제
-   //경매 추가
-   @RequestMapping("/shop/item/addItem/${auctionList.productId}") 
+
+      
+   //경매 추가(로그인 여부 확인)
+   @RequestMapping("/shop/auction/addItem.do/{auctionList.productId}") 
    public String auctionAdd(@PathVariable int productId, ModelMap model) {
 	   return "Product/addAuction";
+   }
+   
+   
+   
+   //경매 수정(로그인 여부 확인)
+   @RequestMapping("/shop/auction/listItem.do") 
+   public String auctionUpdate(@RequestParam("productId") int productId, ModelMap model) {
+	   
+	   return "Product/item";
+   }
+   
+   
+   //경매 삭제(로그인 여부 확인)
+   @RequestMapping("/shop/auction/listItem.do") 
+   public String auctionDelete(@RequestParam("productId") int productId, ModelMap model) {
+	   
+	   return "Product/auction";
    }
 }
