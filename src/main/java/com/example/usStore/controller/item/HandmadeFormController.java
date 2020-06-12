c897a2cd17a60f16a3b10f4c5c376db7c57e5510
jingni <48404998+leejieuns2@users.noreply.github.com>
2020-06-12 14:33:47 +0900

    [add] HandMade View 구현중

--- src/main/java/com/example/usStore/controller/item/HandmadeFormController.java
+++ src/main/java/com/example/usStore/controller/item/HandmadeFormController.java
@@ -8,10 +8,13 @@ import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.usStore.domain.HandMade;
+import com.example.usStore.domain.SecondHand;
 import com.example.usStore.service.facade.ItemFacade;
 
 @Controller
@@ -24,12 +27,11 @@ public class HandmadeFormController {
 		this.itemFacade = itemFacade;
 	}
 
-	@RequestMapping("shop/handMade/listItem.do")
-	public String handleRequest(
-			@RequestParam("productId") int productId,
-			ModelMap model) throws Exception {
-//		PagedListHolder<Item> itemList = new PagedListHolder<Item>(this.itemFacade.getItemListByProduct(productId));
-//		itemList.setPageSize(4);
+	@RequestMapping("/shop/handMade/listItem.do")
+	public String listHandMade (
+			@RequestParam("productId") int productId, ModelMap model) throws Exception {
+		PagedListHolder<HandMade> itemList = new PagedListHolder<HandMade>(this.itemFacade.getHandMadeList());
+		itemList.setPageSize(4);
 		List<HandMade> list = itemFacade.getHandMadeList();
 		model.put("list", list);
 		return "Product/handMade";
@@ -50,5 +52,30 @@ public class HandmadeFormController {
 		model.put("itemList", itemList);
 		model.put("handMade", handMade);
 		return "Product";
+	}
+	
+	@RequestMapping("/shop/handMade/addItem.do")
+	public String goItem(@RequestParam("productId") int productId) {
+	      return "redirect:/shop/item/addItem.do?productId=" + productId;
+	}
+	
+	@ModelAttribute("handMadeForm")
+	public SecondHandForm createSecondHandForm() {
+		return new SecondHandForm();
+	}
+	
+	@RequestMapping("/shop/handMade/addItem/${itemId}")
+	public ModelAndView handleRequest(
+			@PathVariable String itemId,
+			@ModelAttribute("handMadeForm") HandMade handMade 
+			) throws Exception {
+		/*
+		 * if (cart.containsItemId(workingItemId)) {
+		 * cart.incrementQuantityByItemId(workingItemId); } else { boolean isInStock =
+		 * this.petStore.isItemInStock(workingItemId); Item item =
+		 * this.petStore.getItem(workingItemId); cart.addItem(item, isInStock); } return
+		 * new ModelAndView("Cart", "cart", cart);
+		 */
+		return null;
 	}
 }
