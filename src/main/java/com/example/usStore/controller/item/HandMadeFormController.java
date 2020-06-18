1e7c4d92faa6c739e90e6c74d5a08ea8f5f638c2
jingni <48404998+leejieuns2@users.noreply.github.com>
2020-06-19 00:00:31 +0900

    [add][fix] add item, detail item, edit item 마무리 구현 중

--- src/main/java/com/example/usStore/controller/item/HandMadeFormController.java
+++ src/main/java/com/example/usStore/controller/item/HandMadeFormController.java
@@ -1,7 +1,6 @@
 package com.example.usStore.controller.item;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,23 +14,21 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.example.usStore.domain.GroupBuying;
+import com.example.usStore.controller.mypage.UserSession;
 import com.example.usStore.domain.HandMade;
 import com.example.usStore.domain.Item;
 import com.example.usStore.domain.Tag;
 import com.example.usStore.service.facade.ItemFacade;
 
 @Controller
-@SessionAttributes({"handMadeForm", "itemList"})
+@SessionAttributes({"handMadeForm", "itemList", "userSession"})
 public class HandMadeFormController {
 
 	private static final String ADD_HANDMADE_FORM = "product/addHandMade";
@@ -68,7 +65,7 @@ public class HandMadeFormController {
 		return HANDMADE_LIST;
 	}
 	
-	// �럹�씠吏� �꽆�뼱媛덈븣 �떎�뻾�릺�뒗 Controller
+	// 아이템 4개마다 페이지 구분, 다음 페이지나 이전 페이지로 전환하도록 해주는 Controller
 	@RequestMapping("shop/handMade/listItem2.do")
 	public String listHandMade2 (
 			@ModelAttribute("itemList") PagedListHolder<HandMade> itemList,
@@ -82,6 +79,18 @@ public class HandMadeFormController {
 		}
 		model.put("itemList", itemList);
 		return HANDMADE_LIST;
+	}
+	
+	@RequestMapping("/shop/handMade/index.do") // go index(remove sessions)
+	public String goIndex(SessionStatus sessionStatus, HttpServletRequest rq) {
+		System.out.println("go back index.do From [add / edit product]");
+		HttpSession session = rq.getSession(false);
+
+		sessionStatus.setComplete();// groupBuying session close
+		session.removeAttribute("itemForm"); // itemForm session close
+		session.removeAttribute("status"); // edit flag Session close
+
+		return "redirect:/shop/index.do";
 	}
 	
 	////////////////////////////////////////////////
@@ -114,7 +123,7 @@ public class HandMadeFormController {
 	@PostMapping("/shop/handMade/step3.do")		// step2 -> step3 占쎌뵠占쎈짗
 	public String step3(
 			@ModelAttribute("HandMade") HandMadeForm handMadeForm, 
-			HttpServletRequest rq, ItemForm itemForm, Model model) {
+			HttpServletRequest rq, ItemForm itemForm, Model model, BindingResult bindingResult) {
 		
 		System.out.println("step3.do(before check form)");
 		HttpSession session = rq.getSession(false);
@@ -126,6 +135,8 @@ public class HandMadeFormController {
 		
 		System.out.println("handMadeCommand: " + handMadeForm);	//print command toString
 		
+		///// 여기 수정해야 함
+//		handMadeForm.setItemId();
 		model.addAttribute(itemForm);
 		return CHECK_FORM3;	// step3(CHECK_FORM3)
 	}
@@ -141,26 +152,56 @@ public class HandMadeFormController {
 	@PostMapping("/shop/handMade/detailItem.do")		// step3 -> done
 	public String finalAddHandMade(@ModelAttribute("HandMade") HandMadeForm handMadeForm, 
 			ItemForm itemformSession, BindingResult result, Model model, HttpServletRequest rq, 
-			SessionStatus sessionStatus, HandMade handMade) {
+			SessionStatus sessionStatus, HandMade handMade, ModelMap modelMap) {
 		System.out.println("detailItem.do");
 		
 		HttpSession session = rq.getSession(false);
 		itemformSession = (ItemForm) session.getAttribute("itemForm");
+		
+		int status = (int) session.getAttribute("status");
+		UserSession userSession = (UserSession) session.getAttribute("userSession");
+		
 		if(session.getAttribute("itemForm") != null) {
 			System.out.println("itemformSession: " + itemformSession);	// print itemformSession toString
 		}
+	
+		String suppId = userSession.getAccount().getUserId();
+		System.out.println("suppId: " + suppId);
 		
+		if(session.getAttribute("itemForm") != null) {
+			System.out.println("itemformSession: " + itemformSession);	//print itemformSession toString
+		}
 		System.out.println(handMadeForm);
 	
 		//put itemformSession to item
 		Item item = new Item(itemformSession.getUnitCost(), itemformSession.getTitle(), 
-				itemformSession.getDescription(), itemformSession.getQty(), "A", 	// must change userId -> loginCommand.getUserId()
+				itemformSession.getDescription(), itemformSession.getQty(), suppId, 	// must change userId -> loginCommand.getUserId()
 				itemformSession.getProductId());
 		
-		itemFacade.insertItem(item);	// -> generate itemId, insert DB
-		
+		if(status != 0) {
+			item.setItemId(status);
+			item.setViewCount(itemformSession.getViewCount());
+			itemFacade.updateItem(item);
+			System.out.println("itemUpdated" + item);
+		}
+		else {
+			itemFacade.insertItem(item);	// -> generate itemId, insert DB
+		}
+
 		System.out.println("itemId: " + item.getItemId());	//print itemformSession toString
 		
+		if(status != 0) {
+			List<Tag> tags = itemFacade.getTagByItemId(status);
+			System.out.println("tag size : " + tags.size());	//0
+			if(tags.size() > 0) {
+				itemFacade.deleteTag(status);
+				tags.removeAll(tags);
+				
+				System.out.println("removed tags, tag size : " + tags.size());
+				List<Tag> t = itemFacade.getTagByItemId(status);
+				System.out.println("deleted, tag size : " + t.size());
+			}
+		}
 		//generate tags(only have tagName)
 		itemformSession.makeTags(item.getItemId(), itemformSession.getTag1());	//if(tag != null && "") then addTags
 		itemformSession.makeTags(item.getItemId(), itemformSession.getTag2());
@@ -180,12 +221,26 @@ public class HandMadeFormController {
 		handMade.setItemId(item.getItemId());
 		handMade.setListPrice(handMadeForm.getListPrice());
 		
-		itemFacade.insertHandMade(handMade);	// insert DB
+		if(status != 0) {
+			itemFacade.updateHandMade(handMade); // update DB
+		}
+		else {
+			itemFacade.insertHandMade(handMade);	// insert DB
+		}
 		
-		model.addAttribute("handMade", handMade);
-		model.addAttribute("productId", item.getProductId());
+		handMade.setItemId(status);
+		handMade.setListPrice(handMadeForm.getListPrice());
 		
-		sessionStatus.setComplete();	// session close
+		if(status != 0) {
+			itemFacade.updateHandMade(handMade);
+		}
+		else {
+			itemFacade.insertHandMade(handMade);	// insert DB
+		}
+	
+		sessionStatus.setComplete();	// groupBuying, editStatus session close
+		session.removeAttribute("itemForm");	//itemForm session close
+		
 		return DETAIL;
 	}
 	
@@ -195,13 +250,98 @@ public class HandMadeFormController {
 	
 	@RequestMapping("/shop/handMade/viewItem.do")
 	public String handMadeView(@RequestParam("itemId") int itemId, 
-			@RequestParam("productId") int productId, ModelMap model) {
-		HandMade handMade = this.itemFacade.getHandMadeById(itemId);
+			@RequestParam("productId") int productId,
+			HttpServletRequest rq, Model model, ModelMap modelMap) {
 		
+		HttpSession session = rq.getSession(false);
+		
+		if(session.getAttribute("userSession") != null) {
+			UserSession userSession = (UserSession) session.getAttribute("userSession");
+			String suppId = userSession.getAccount().getUserId();
+			
+			System.out.println("suppId: " + suppId);
+			model.addAttribute("suppId", suppId);
+		}
+		
+		Item item = itemFacade.getItem(itemId);
+		item.setViewCount(item.getViewCount() + 1);
+		System.out.println("object's viewcount ++ ? : " + item);
+		
+		itemFacade.updateItem(item);	// update : viewCount++
+		System.out.println("updated item data: " + itemFacade.getItem(itemId).toString());
+				
+		HandMade handMade = this.itemFacade.getHandMadeById(itemId);
+		System.out.println(handMade);
+		
+		List<Tag> tags = new ArrayList<Tag>();
+		tags = itemFacade.getTagByItemId(handMade.getItemId());
+		
+		model.addAttribute("productId", productId);
+		modelMap.addAttribute("tags", tags);
 		model.addAttribute("handMade", handMade);
 		model.addAttribute("productId", productId);
 		return DETAIL;
 	}
+	
+	////////////////////////////////////////////////
+	// HandMade Edit Controller
+	////////////////////////////////////////////////
+	
+	@RequestMapping("/shop/handMade/edit.do") //edit Item
+	public String editItem(ItemForm itemForm, 
+					Item item,	@RequestParam("itemId") int itemId, HttpServletRequest rq)
+	{
+		HttpSession session = rq.getSession(true);
+		session.setAttribute("itemForm", itemForm);
+		session.setAttribute("status", itemId);
+		
+		List<Tag> tags = new ArrayList<Tag>();
+		tags = itemFacade.getTagByItemId(itemId);
+		
+		int status = (int) session.getAttribute("status");
+		System.out.println("edit.do");
+		System.out.println("itemId(status) : " + status);
+		
+		item = itemFacade.getItem(itemId);
+		
+		ItemForm itemFormSession = (ItemForm) session.getAttribute("itemForm");
+		itemFormSession.setUnitCost(item.getUnitCost());
+		itemFormSession.setTitle(item.getTitle());
+		itemFormSession.setDescription(item.getDescription());
+		itemFormSession.setQty(item.getQty());
+		itemFormSession.setViewCount(item.getViewCount());
+				
+		switch(tags.size()) {
+			case 1: itemFormSession.setTag1(tags.get(0).getTagName());
+					break;
+					
+			case 2: itemFormSession.setTag1(tags.get(0).getTagName());
+					itemFormSession.setTag2(tags.get(1).getTagName());
+					break;
+					
+			case 3: itemFormSession.setTag1(tags.get(0).getTagName());
+					itemFormSession.setTag2(tags.get(1).getTagName());
+					itemFormSession.setTag3(tags.get(2).getTagName());
+					break;
+			
+			case 4: itemFormSession.setTag1(tags.get(0).getTagName());
+					itemFormSession.setTag2(tags.get(1).getTagName());
+					itemFormSession.setTag3(tags.get(2).getTagName());
+					itemFormSession.setTag4(tags.get(3).getTagName());
+					break;
+			
+			case 5: itemFormSession.setTag1(tags.get(0).getTagName());
+					itemFormSession.setTag2(tags.get(1).getTagName());
+					itemFormSession.setTag3(tags.get(2).getTagName());
+					itemFormSession.setTag4(tags.get(3).getTagName());
+					itemFormSession.setTag5(tags.get(4).getTagName());
+					break;
+		}
+		System.out.println(itemFormSession);
+		
+		return "redirect:/shop/item/addItem.do?productId=" + item.getProductId();
+	}
+	
 	
 	////////////////////////////////////////////////
 	// HandMade Delete Controller
--- src/main/webapp/WEB-INF/jsp/product/viewHandMade.jsp
+++ src/main/webapp/WEB-INF/jsp/product/viewHandMade.jsp
@@ -39,13 +39,17 @@
 <table id="main-menu">
   <tr>
     <td>
-      <a href='<c:url value="/shop/handMade/viewItem.do">
-        <c:param name="productId" value="${handMade.productId}"/></c:url>'>
-        <b><font color="black" size="2">
-          &lt;&lt; Go to List</font></b></a>
+	    <a href='<c:url value="/shop/index.do"/>'>
+	        <b><font color="black" size="2">&lt;&lt; Go to Index</font></b>
+	    </a><br><br>
+	    <a href='<c:url value="/shop/handMade/listItem.do">
+    				<c:param name="productId" value="${handMade.productId}" />
+    			</c:url>'>
+	        <b><font color="black" size="2">&lt;&lt; Go to List</font></b>
+	    </a><br>
     </td>
   </tr>
-</table>
+</table> 
 
 <body>
 	<table id="item-detail" style="margin-left: auto; margin-right: auto;">
@@ -95,20 +99,12 @@
    		<tr>
    			<th style="border-right: 1px solid black;"><font color=blue>#</font>관련태그</th>
    			<td>
-   			<%-- <c:forEach var="tag" items="${tag}">	<!-- tag 테이블 이용 -> 해당 itemId를 어떻게 연결하지? -->
-   				<a href="
-					<c:url value='/searchTag'>	<!-- tag검색 결과 페이지로 이동 -->
-					  <c:param name="tagName" value="${tag.tagName}"/>
-				  	</c:url>
-				">#${tag.tagName}</a>&nbsp;
-   			</c:forEach> --%>
-   			<a href="
-					<c:url value='/searchTag/${gb.tagName}'/>	<!-- tag검색 결과 페이지로 이동 -->
-				">#과잠</a>&nbsp;
-				
-			<a href="
-					<c:url value='/searchTag/${gb.tagName}'/>	<!-- tag검색 결과 페이지로 이동 -->
-				">#단체복</a>&nbsp;
+   			<c:forEach var="tag" items="${tags}">        
+	   			<a href="
+						<c:url value='/searchTag/${handMade.itemId}'/>	
+					">#${tag.tagName}
+				</a>&nbsp;
+			</c:forEach>
    			</td>
    		</tr>
    		<tr>
@@ -124,10 +120,10 @@
    			<th style="border-right: 1px solid black;">수량 </th> 
 	   		<td>
 	   		<c:if test="${handMade.qty <= 0}">
-			        <font color="red" size="2"><i>Back ordered.</i></font>
+			        <font color="red" size="2"><i>품절</i></font>
 			      </c:if> 
 			      <c:if test="${handMade.qty > 0}">
-			        <font size="2"><fmt:formatNumber value="${handMade.qty}" /> in stock.</font>
+			        <font size="2"><fmt:formatNumber value="${handMade.qty}" /> 개 남았습니다.</font>
 			      </c:if>
 	   		</td>
    		</tr>
@@ -136,32 +132,33 @@
    			<td colspan="2" style="border-bottom: none;">
    			<span>
 		   			<a href="
-							<c:url value='/shop/addItemToCart.do/${gb.itemId}'/>	<!-- 로그인 여부 따지기 -->
+							<c:url value='/shop/addItemToCart.do/${handMade.itemId}'/>	<!-- 로그인 여부 따지기 -->
 					">장바구니 추가</a>
 				</span>
 				&nbsp;&nbsp;&nbsp;
 				<span>
-					<a href="
-							<c:url value='/order/${gb.itemId}'/>	<!-- 로그인 여부 따지기 -->
-					">공동구매 참여</a>
-				</span>
-				&nbsp;&nbsp;&nbsp;
-				<span>
    				<a href="
-							<c:url value='/note/${gb.itemId}'/>	<!-- 로그인 여부 따지기 -->
+							<c:url value='/note/${handMade.itemId}'/>	<!-- 로그인 여부 따지기 -->
 				">쪽지 보내기</a>
 				</span>
    			
    			</td>
    		</tr>
-<%--    		<c:if test="${sh.suppId==session.userId}"> <!-- 로그인시 실행 --> --%>
+   		<c:if test="${handMade.userId eq suppId}"> <!-- ë¡ê·¸ì¸ì ì¤í -->
 	   		<tr>
 		   		<td colspan="2" style="text-align: right; padding: 0px; font-size: small; border-bottom: none; border-top: 1px solid black;">
-				   <a href="<c:url value='/shop/handMade/editItem.do?itemId=${handMade.itemId}'/>">[게시물 수정하기]</a>
-				   <a href="<c:url value='/shop/handMade/deleteItem.do?productId=${handMade.productId}&itemId=${handMade.itemId}'/>"> [게시물 삭제하기]</a>
+				   <a href="<c:url value='/shop/handMade/edit.do'>
+				   				<c:param name="itemId" value="${handMade.itemId}" />
+				   			</c:url>
+				   			">[게시물 수정하기]</a>
+				   <a href="<c:url value='/shop/handMade/deleteItem.do'>
+				   				<c:param name="productId" value="${handMade.productId}" />
+				   				<c:param name="itemId" value="${handMade.itemId}" />
+				   			</c:url>
+				   			"> [게시물 삭제하기]</a>
 				</td>
-			</tr>
-<%-- 		</c:if> --%>
+			 </tr>
+		</c:if>
    	</table>
    	<br><br>
 </body>
