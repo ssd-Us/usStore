629e552c45eb13dd4dbab750d349a4c8e0dbdd51
angelatto <pudri109@gmail.com>
2020-06-19 20:12:07 +0900

    [commit] MybatisAuctionDao와 ItemImpl searchItemList메소드 삭제

--- src/main/java/com/example/usStore/service/impl/ItemImpl.java
+++ src/main/java/com/example/usStore/service/impl/ItemImpl.java
@@ -92,11 +92,6 @@ public class ItemImpl implements ItemFacade {
 	}
 	
 	@Override
-	public List<Item> searchItemList(String keywords) {
-		return itemDao.searchItemList(keywords);
-	}
-	
-	@Override
 	public void deleteItem(int itemId) {
 		// TODO Auto-generated method stub
 		itemDao.deleteItem(itemId);
@@ -348,5 +343,11 @@ public class ItemImpl implements ItemFacade {
 	@Override
 	public String getUserIdByItemId(int itemId) {
 		return itemDao.getUserIdByItemId(itemId);
+	}
+
+	@Override
+	public List<Item> searchItemList(String keywords) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
