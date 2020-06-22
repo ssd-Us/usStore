dc2bdd359567dbbdf89729a23ee0e3c02f3455d2
ss1141204 <ss1141204@gmail.com>
2020-06-23 03:13:34 +0900

    [fix] item 도메인 다시 merge

--- src/main/java/com/example/usStore/domain/Item.java
+++ src/main/java/com/example/usStore/domain/Item.java
@@ -16,7 +16,7 @@ public class Item implements Serializable {
 	private String description;
 	private int viewCount;
 	private int qty;
-	private List<Tag> tags;
+	private List<Tag> tags = new ArrayList<Tag>();
 	private String userId; // (FK)
 	private int productId; // (FK)
 
@@ -103,6 +103,13 @@ public class Item implements Serializable {
 	public void setTags(List<Tag> tags) {
 		this.tags = tags; 
 	}
+	
+	public void makeTags(int itemId, String tagName) {	//itemId�몴占� tag占쎌벥 itemId嚥∽옙, tagName(tag1, 2, 3, 4, 5) 筌욑옙占쎌젟 , tagId占쎈뮉 占쎌뿫占쎌벥占쎌벥 占쎈땾 0 占쎌몵嚥∽옙 筌욑옙占쎌젟
+		if(tagName != null && tagName.trim() != "") {
+			Tag tag = new Tag(itemId, tagName);
+			tags.add(tag);
+		}	
+	  }
 	
 	@Override
 	public String toString() {
